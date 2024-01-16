e26a3452ec2bc68cf96d788439af59ef72072259
Bruno de Moura <bruno.moura@ziyon.fr>
2024-01-16 14:41:10 -0300

    Initial project creation and configuration (ViewCode).

---
+++ AppAlcoolOuGasolina/AppAlcoolOuGasolina/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppAlcoolOuGasolina
+//
+//  Created by Bruno Moura on 16/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
